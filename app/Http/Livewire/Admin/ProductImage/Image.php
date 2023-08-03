8a70cacea39c979ccf01b18e8e501a19188ead62
elgun <elgun.huseynov@azeurotel.az>
2023-08-03 16:52:31 +0400

    Product read, edit update and delete with images in laravel

---
+++ app/Http/Livewire/Admin/ProductImage/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\Admin\ProductImage;
+
+use App\Models\Product;
+use App\Models\Product_Image;
+use Livewire\Component;
+
+class Image extends Component
+{
+/*    public $id;
+    public function render()
+    {
+
+
+        $products=Product_Image::findOrFail(7);
+        return view('livewire.admin.product-image.image',compact('products'));
+    }*/
+}
---
+++ resources/views/livewire/admin/product-image/image.blade.php
@@ -0,0 +1,20 @@
+<div>
+
+{{--    <div class="">
+        @if($products->productImages)
+            <div class="row">
+                @foreach($products->productImages as $image)
+                    <div class="col-md-1">
+                        <img src="{{asset($image->image)}}" style="width: 80px;height: 80px" alt="img" class="me-4 border">
+                        <a href="{{route('product.ProductImageDel',$image->id)}}" class="d-block  btn-outline-danger align text-center">
+                            <i class='bx bx-x-circle bx bxs-like bx-md' ></i>
+                               <i class='bx bxs-x-circle bx bxs-like bx-lg'></i>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <h4> No added image</h4>
+        @endif
+    </div>--}}
+</div>
